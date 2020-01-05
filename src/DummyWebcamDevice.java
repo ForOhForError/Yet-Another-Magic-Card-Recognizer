3c631cda196b82e3227405ab3c281cbacddbc8a8
ForOhForError <16399039+ForOhForError@users.noreply.github.com>
2020-01-05 18:00:30 -0500

    Made simulated webcam look less like shit

--- src/DummyWebcamDevice.java
+++ src/DummyWebcamDevice.java
@@ -1,6 +1,7 @@
 import com.github.sarxos.webcam.WebcamDevice;
 
 import java.awt.image.BufferedImage;
+import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
 import java.util.Random;
@@ -14,6 +15,7 @@ import java.awt.Dimension;
 
 class DummyWebcamDevice implements WebcamDevice {
 
+    private BufferedImage buffer;
     private BufferedImage display;
     Graphics2D g;
     private static final int WIDTH = 640;
@@ -22,24 +24,29 @@ class DummyWebcamDevice implements WebcamDevice {
     private static final int BOUNCE_SIZE = 50;
     private static final int BOUNCE_SIZE2 = BOUNCE_SIZE * 2;
 
-    private static final int MAX_VEL = 3;
+    private static final double MAX_VEL = 100;
 
-    private BufferedImage bounce;
+    private Image bounce;
     private double bounceX = WIDTH / 2;
     private double bounceY = HEIGHT / 2;
 
     private double xvel;
     private double yvel;
 
+    private long time;
+
     private Dimension[] dimensions;
 
     boolean open;
 
     public DummyWebcamDevice() {
+        time = System.currentTimeMillis();
+        buffer = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
         display = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
-        g = display.createGraphics();
+        g = buffer.createGraphics();
         try {
-            bounce = ImageIO.read(new File("res/YamCR.png"));
+            bounce = ImageIO.read(new File("res/YamCR.png"))
+                .getScaledInstance(BOUNCE_SIZE2, BOUNCE_SIZE2, BufferedImage.SCALE_SMOOTH);
         } catch (IOException e) {
             bounce = null;
         }
@@ -90,14 +97,19 @@ class DummyWebcamDevice implements WebcamDevice {
     {
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, WIDTH, HEIGHT);
-        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-        g.drawImage(bounce, (int)bounceX-BOUNCE_SIZE, (int)bounceY-BOUNCE_SIZE, BOUNCE_SIZE2, BOUNCE_SIZE2, null);
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g.drawImage(bounce, (int)bounceX-BOUNCE_SIZE, (int)bounceY-BOUNCE_SIZE, null);
+
+        display.getGraphics().drawImage(buffer, 0, 0, null);
     }
 
     private void update()
     {
-        bounceX += xvel;
-        bounceY += yvel;
+        long now = System.currentTimeMillis();
+        double delta = (now-time)/1000.0;
+        time = now;
+        bounceX += xvel*delta;
+        bounceY += yvel*delta;
         if(bounceX > WIDTH - BOUNCE_SIZE)
         {
             bounceX = WIDTH - BOUNCE_SIZE;
